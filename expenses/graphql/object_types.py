074101b082d72d26335fbd2198b45f88732b44e3
Chiron1991 <mail@chiron-online.de>
2020-11-29 16:34:25 +0100

    make pymoneyed's Money objects resolvable by graphene by adding a custom object type for it

--- expenses/graphql/object_types.py
+++ expenses/graphql/object_types.py
@@ -1,18 +1,53 @@
+import graphene
 from graphene_django import DjangoObjectType
 
 from expenses.models import Tag, CostCenter, VariableCost
+
+_CURRENCY_SYMBOLS = {
+    'EUR': '€',
+    'USD': '$',
+    'GBP': '£',
+}
+
+
+class MoneyType(graphene.ObjectType):
+    """
+    Used to represent moneyed's Money objects (as used by django-money's MoneyField).
+    todo: tests
+    """
+    amount = graphene.String()
+    currencyName = graphene.String()
+    currencyCode = graphene.String()
+    currencySymbol = graphene.String()
+
+    def resolve_amount(self, info):
+        return str(self.amount)
+
+    def resolve_currencyName(self, info):
+        return self.currency.name
+
+    def resolve_currencyCode(self, info):
+        return self.currency.code
+
+    def resolve_currencySymbol(self, info):
+        return _CURRENCY_SYMBOLS.get(self.currency.code)
 
 
 class TagType(DjangoObjectType):
     class Meta:
         model = Tag
+        fields = ('title',)
 
 
 class CostCenterType(DjangoObjectType):
     class Meta:
         model = CostCenter
+        fields = ('name',)
 
 
 class VariableCostType(DjangoObjectType):
+    cost = graphene.Field(MoneyType)
+
     class Meta:
         model = VariableCost
+        fields = ('date', 'cost', 'notes', 'cost_center', 'tags')

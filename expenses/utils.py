8b45b66d76770ed66800ffda341938509c98094a
Chiron1991 <mail@chiron-online.de>
2020-12-25 20:43:54 +0100

    add GraphQLTestCase

--- expenses/utils.py
+++ expenses/utils.py
@@ -4,10 +4,13 @@ import pprint
 import sys
 from http import HTTPStatus
 
+from django.contrib.auth import get_user_model
 from django.core.serializers.json import DjangoJSONEncoder
 from django.http import HttpResponse
+from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils.html import format_html
+from graphene_django.utils.testing import graphql_query
 
 
 def prettify(obj, stream=sys.stdout, **format_kwargs):
@@ -53,3 +56,33 @@ class AuthenticationRequiredMixin:
         if not request.user.is_authenticated:
             return HttpResponse(status=HTTPStatus.UNAUTHORIZED)
         return super().dispatch(request, *args, **kwargs)
+
+
+class GraphQLTestCase(TestCase):
+    """
+    Base class for testing this project's GraphQL API.
+    """
+
+    def setUp(self):
+        self.superuser = get_user_model().objects.create_superuser(username='root', password='root')
+        self.user = get_user_model().objects.create_user(username='john', password='john')
+
+        self.client = Client()
+
+        super().setUp()
+
+    def query(self, query, user=None, **kwargs):
+        if user is not None:
+            self.client.login(username=user.username, password=user.username)
+
+        response = graphql_query(
+            query,
+            client=self.client,
+            graphql_url='/graphql/',
+            **kwargs,
+        )
+
+        if user is not None:
+            self.client.logout()
+
+        return response
